597d717daa22c6e0ee7360120fd2131e68c4440e
amin <aminsheikhi@gmail.com>
2023-03-31 17:06:03 +0330

    feat(Email): move src into to lib and implement html render

--- lib/src/email/email.module.ts
+++ lib/src/email/email.module.ts
@@ -1,12 +1,13 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { resolveEmailServiceImp, resolveHtmlRenderServiceImp, resolveOptionsProvider } from './email.providers';
 import { EmailModuleOptions } from './email.type';
-import { EMAIL_SERVICE } from './email.constants';
+import { EMAIL_SERVICE, HTML_RENDER_SERVICE } from './email.constants';
 
 @Module({
   providers: [],
   exports: [
-    EMAIL_SERVICE
+    EMAIL_SERVICE,
+    HTML_RENDER_SERVICE
   ]
 })
 export class EmailModule {
